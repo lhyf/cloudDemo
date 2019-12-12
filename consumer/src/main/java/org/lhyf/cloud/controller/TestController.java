1b674eacd8b92227e23c76ebdc99a42db3a647cf
yangf <lhyf@outlook.com>
2019-12-12 23:46:17 +0800

    .

--- consumer/src/main/java/org/lhyf/cloud/controller/TestController.java
+++ consumer/src/main/java/org/lhyf/cloud/controller/TestController.java
@@ -1,24 +1,24 @@
 package org.lhyf.cloud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-/**
- * @author yangfan
- * @date 2019/12/12 15:12
- * @Description
- */
-@RestController
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.client.RestTemplate;
+//
+///**
+// * @author yangfan
+// * @date 2019/12/12 15:12
+// * @Description
+// */
+//@RestController
 public class TestController {
-
-    @Autowired
-    private  RestTemplate restTemplate;
-
-    @GetMapping(value = "/echo/{str}")
-    public String echo(@PathVariable String str) {
-        return restTemplate.getForObject("http://provider/echo/" + str, String.class);
-    }
+//
+//    @Autowired
+//    private  RestTemplate restTemplate;
+//
+//    @GetMapping(value = "/echo/{str}")
+//    public String echo(@PathVariable String str) {
+//        return restTemplate.getForObject("http://provider/echo/" + str, String.class);
+//    }
 }
